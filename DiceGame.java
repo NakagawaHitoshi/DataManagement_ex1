94ad048650d64c1ef835e1fee2018e47d390c1e1
NakagawaHitoshi <136040516+NakagawaHitoshi@users.noreply.github.com>
2023-06-09 12:39:10 +0900

    Greetings Added

--- DiceGame.java
+++ DiceGame.java
@@ -1,7 +1,14 @@
 import java.util.Random;
+import java.util.scanner;
 
 class DiceGame{
     public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What your name?");
+        String name = sc.nextLine();
+        System.out.println("Hello "+name+"!");
+        
         Random rand = new Random();
         int dice1 = rand.nextInt(6)+1;
         int dice2 = rand.nextInt(6)+1;
